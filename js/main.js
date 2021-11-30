f56f7f2c5d1fe3fc72cd467b0578fb2c01717e04
phoekoby <phoekoby@mail.ru>
2021-11-30 12:38:45 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+});

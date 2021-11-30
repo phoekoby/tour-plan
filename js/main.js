a8dee736d5752489a1addb68d2c2b1ad08d77ffd
phoekoby <phoekoby@mail.ru>
2021-12-01 02:55:03 +0300

    create: responsive mab and booking

--- js/main.js
+++ js/main.js
@@ -21,16 +21,26 @@ $(document).keydown(function(e) {
 });
 
 
-function _resizeDiv(options){
-  var ratio=options.ratio;
-      var elem = $(options.element);
-      elem.css({'max-height':elem.width()*ratio})
-  
-  }
-  $(window).resize(function(){
-      var elem = document.getElementById('slider');
-      _resizeDiv({
-        ratio:0.6428,
-        element:elem
-      })
-  })
+// function _resizeDiv(options){
+//   var ratio=options.ratio;
+//       var elem = $(options.element);
+//       if(document.documentElement)
+//       if(document.documentElement.clientWidth<=768){
+//         console.log(1);
+//         elem.css({'max-height':elem.width()*ratio,
+//                   'max-width': 700})
+//       }else if(document.documentElement.clientWidth>768){
+//         console.log(elem.width());
+//         elem.css({'max-height':elem.width()*0.62,
+//                   'max-width': 728,
+//                   })
+//       }
+    
+//   }
+//   $(window).resize(function(){
+//       var elem = document.getElementById('slider');
+//       _resizeDiv({
+//         ratio:0.6428,
+//         element:elem
+//       })
+//   })
